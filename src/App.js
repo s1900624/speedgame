d3521cf9e6b418af56543fbd75e667b5fb42860c
Your Name <you@example.com>
2019-11-24 13:13:12 +0200

    Add; new css style and logic

--- src/App.js
+++ src/App.js
@@ -1,40 +1,37 @@
 import React, { Component } from 'react';
 import Circle from './components/circle';
 import GameOver from './components/game-over';
-let colors = ["blue", "red", "yellow", "green"];
+import './App.css'
+const colors = ['black', 'black', 'black', 'black'];
+const hbcr = ['H', 'B', 'C', 'R'];
 
 function getRandomInt(min, max) {
   return (Math.floor(Math.random() * (max - min + 1)) + min);
 }
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      result: 0,
-      gameOver: false,
-      activeCircle: 0,
-      tries: 5
-    }
-  }
+  state = {
+    result: 0,
+    gameOver: false,
+    activeCircle: 0,
+    tries: 5,
+  };
 
   interval = null;
   pace = 1000;
 
   startGame = () => {
     this.setActiveCircle();
-  }
+  };
 
   endGame = () => {
     clearInterval(this.interval);
     this.setState({
       gameOver: true
     })
-  }
+  };
 
   clickHandler = (id) => {
-    console.log(id);
-    console.log(this.state.activeCircle);
     if(this.state.activeCircle === id) {
       this.pace *= 0.95;
       this.setState({
@@ -52,7 +49,7 @@ class App extends Component {
         this.setActiveCircle();
       }
     }
-  }
+  };
 
   setActiveCircle = () => {
     let nextActive = null;
@@ -65,7 +62,7 @@ class App extends Component {
     this.setState({
       activeCircle: nextActive
     });
-  }
+  };
 
   closeHandler = () => {
     this.setState({
@@ -74,32 +71,48 @@ class App extends Component {
       activeCircle: 0,
       tries: 5
     });
-  }
+  };
 
   render() {
-    let circleList = colors.map((color, index) => {
+    const circleList = colors.map((color, index) => {
       return (
-          <Circle clickHandler={() => this.clickHandler((index+1))} key={index} bgColor={color} active={this.state.activeCircle === (index + 1) ? "active" : ""} />
+          <Circle
+              key={index}
+              bgColor={color}
+              clickHandler={() => this.clickHandler((index+1))}
+              active={this.state.activeCircle === (index + 1) ? "active" : ""}
+              text={hbcr[index]}
+          />
       );
     });
 
     return (
         <div className="App">
           <header>
-            <h1>Speed Game</h1>
-            <p>Your score: {this.state.result} | tries left: {this.state.tries }</p>
-          </header>
-          <main>
-            {this.state.gameOver && <GameOver score={ this.state.result} closeHandler={this.closeHandler} />}
-            {!this.state.gameOver && <><section>
-              {circleList}
-            </section>
-              <section>
+            {!this.state.gameOver &&
+            <>
+              <div className='start-container'>
+                <h1>Speed Game</h1>
+                <p>Your score: {this.state.result} | tries left: {this.state.tries}</p>
                 <div>
-                  <button class="button-green" onClick={this.startGame}>Start Game</button>
+                  <button className="button-green" onClick={this.startGame}>Start Game</button>
                   <button className="button-red" onClick={this.endGame}>End Game</button>
                 </div>
-              </section></>}
+              </div>
+              <div className='circle-container'>
+                {circleList}
+              </div>
+            </>
+            }
+          </header>
+          <main>
+            {this.state.gameOver &&
+            <div className='exit-container'>
+              <GameOver
+                  score={ this.state.result}
+                  closeHandler={this.closeHandler}
+              />
+            </div>}
           </main>
         </div>
     );
--- src/components/circle/circle.js
+++ src/components/circle/circle.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './circle.css'
 
-const Circle = ({active, bgColor, clickHandler}) => {
+const Circle = ({active, bgColor, clickHandler, text}) => {
     return (
-        <div onClick={clickHandler} className={'circle ' + bgColor + ' ' + active}>
-
+        <div
+            onClick={clickHandler}
+            className={`circle ${bgColor} ${active}`}>
+            {text}
         </div>
     )
 }
