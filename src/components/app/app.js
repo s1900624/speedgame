0ff9f532c406d73375179202afb038fac73bb13b
Your Name <you@example.com>
2019-11-24 13:48:11 +0200

    Refactoring: app in component

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import Circle from './components/circle';
-import GameOver from './components/game-over';
-import './App.css'
+import Circle from '../circle';
+import GameOver from '../game-over';
+import './app.css'
 const colors = ['black', 'black', 'black', 'black'];
 const hbcr = ['H', 'B', 'C', 'R'];
 
@@ -14,11 +14,11 @@ class App extends Component {
     result: 0,
     gameOver: false,
     activeCircle: 0,
-    tries: 5,
+    attemps: 5,
+    speed: 1000,
   };
 
   interval = null;
-  pace = 1000;
 
   startGame = () => {
     this.setActiveCircle();
@@ -32,22 +32,17 @@ class App extends Component {
   };
 
   clickHandler = (id) => {
-    if(this.state.activeCircle === id) {
-      this.pace *= 0.95;
+    if (this.state.activeCircle === id) {
       this.setState({
-        result: this.state.result += 1
+        result: this.state.result += 1,
+        speed: Math.round(this.state.speed * 0.7),
       });
       this.setActiveCircle();
     } else {
       this.setState({
-        tries: this.state.tries -= 1
+        attemps: this.state.attemps -= 1
       });
-      if(this.state.tries <= 0) {
-        this.endGame();
-      }
-      else {
-        this.setActiveCircle();
-      }
+      this.state.attemps <= 0 ? this.endGame() : this.setActiveCircle();
     }
   };
 
@@ -58,7 +53,7 @@ class App extends Component {
       nextActive = getRandomInt(1, 4);
     } while(this.state.activeCircle === nextActive);
 
-    this.interval = setTimeout(this.setActiveCircle.bind(this), this.pace);
+    this.interval = setTimeout(this.setActiveCircle.bind(this), this.state.speed);
     this.setState({
       activeCircle: nextActive
     });
@@ -69,7 +64,8 @@ class App extends Component {
       gameOver: false,
       result: 0,
       activeCircle: 0,
-      tries: 5
+      attemps: 5,
+      speed: 1000,
     });
   };
 
@@ -93,7 +89,9 @@ class App extends Component {
             <>
               <div className='start-container'>
                 <h1>Speed Game</h1>
-                <p>Your score: {this.state.result} | tries left: {this.state.tries}</p>
+                <p>Your score: {this.state.result}</p>
+                <p>Attemps: {this.state.attemps}</p>
+                <p>Speed: {this.state.speed}</p>
                 <div>
                   <button className="button-green" onClick={this.startGame}>Start Game</button>
                   <button className="button-red" onClick={this.endGame}>End Game</button>
