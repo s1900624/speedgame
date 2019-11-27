ba91c531b0cf6304989bdeb3889fff40064438cd
Your Name <you@example.com>
2019-11-27 16:33:12 +0200

    Updates

---
+++ precache-manifest.3978a2fef68964e63c8d619619b5bf55.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "482dd04d0b9a605528b5bfd085f4fa0d",
+    "url": "/speedgame/index.html"
+  },
+  {
+    "revision": "8f13f2ca18a4cd59ac56",
+    "url": "/speedgame/static/css/main.d38cd65a.chunk.css"
+  },
+  {
+    "revision": "5e5c6aaffb9d4f2b8da9",
+    "url": "/speedgame/static/js/2.18490dc4.chunk.js"
+  },
+  {
+    "revision": "8f13f2ca18a4cd59ac56",
+    "url": "/speedgame/static/js/main.55c02abd.chunk.js"
+  },
+  {
+    "revision": "a8f9f7c1f3cece9ad7fc",
+    "url": "/speedgame/static/js/runtime-main.a80dad9f.js"
+  }
+]);
